401e371743898891bc553b018d46edcda94d6c7f
GHOST-CESC <90934787+CH4RL5@users.noreply.github.com>
2024-04-20 19:17:47 -0600

    changes

--- index.php
+++ index.php
@@ -5,11 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bulma.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link r <link rel="stylesheet" href="styles.css">
     <title>Down</title>
 </head>
@@ -21,16 +18,17 @@
             <div class="row justify-content-sm-center h-100">
                 <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
                     <div class="text-center my-5">
-                        <a href="index.html"><img src="./images/Logo-2.jpg" alt="logo" width="100"></a>
+                        <a href="index.php"><img src="./images/Logo-2.jpg" alt="logo" width="100"></a>
                     </div>
                     <div class="card shadow-lg">
                         <div class="card-body p-5">
                             <h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
                             <form method="POST" class="" action=" " autocomplete="off">
+
                                 <div class="mb-3">
+
                                     <label class="mb-2 text-muted" for="email">CURP</label>
-                                    <input id="curp" type="text" class="form-control" name="curp" value="" required
-                                        autofocus>
+                                    <input id="curp" type="text" class="form-control" name="curp" value="" required autofocus>
                                 </div>
 
                                 <div class="d-flex align-items-center">
@@ -38,7 +36,7 @@
                                         <input type="checkbox" name="remember" id="remember" class="form-check-input">
                                         <label for="remember" class="form-check-label">Remember Me</label>
                                     </div>
-                                    <button type="submit" class="button is-warning ms-auto">
+                                    <button type="submit" name="btnLogin" class="button is-warning ms-auto">
                                         Login
                                     </button>
                                 </div>
@@ -46,7 +44,7 @@
                         </div>
                         <div class="card-footer py-3 border-0">
                             <div class="text-center">
-                                Don't have an account? <a href="./content/register.html" class="text-dark">Create
+                                Don't have an account? <a href="./content/register.php" class="text-dark">Create
                                     One</a>
                             </div>
                         </div>
@@ -62,7 +60,10 @@
     <audio autoplay loop>
         <source src="./audios/shrek-somebodycena.mp3" type="audio/mp3">
     </audio> -->
+    <?php
 
+    include("./db/controller.php");
+    ?>
 </body>
 
 </html>
