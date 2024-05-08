0fbede3f792d984f9b03b0e41d298b4ae07cfb8f
juan Sánchez Santillán <80808819+Osojuan@users.noreply.github.com>
2024-05-08 16:22:59 -0600

    Add files via upload

---
+++ DownjuegosPrueba/content/memorama.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+   <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
+   <link rel="stylesheet" href="../css/stylesmemorama.css">
+</head>
+
+<body>
+   <div id="stats">0 intentos</div>
+   <div id="wrapper">
+      <div id="game"></div>
+   </div>
+  
+   <header class="bg_animate">
+        <div class="header_nav">
+       
+        </div>
+
+
+
+        <div class="burbujas">
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+        </div>
+    </header>
+
+   <script src="../js/scriptmemorama.js"></script>
+
+   <a href="../content/home-user-l.php" class="rounded-corner-button">Ir al inicio</a>
+
+   <audio id="audioAplausos" src="../audios/mario.mpeg"></audio>
+
+   <button id="restartButton">Reiniciar juego</button>
+
+</body>
+
+</html>
